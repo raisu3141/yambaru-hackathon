d4bd3961c644203bee4548947fb82072efe10ceb
raisu3141 <yuuta.matayoshi1413@gmail.com>
2024-02-26 17:48:28 +0900

    画面サイズのfixを適用

--- lib/pages/timetable_page.dart
+++ lib/pages/timetable_page.dart
@@ -58,212 +58,239 @@ class _MyWidgetState extends State<Timetable> with SingleTickerProviderStateMixi
       body: TabBarView(
         controller: _tabController,
         children: [
-          Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 16,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '$formattedTime $nowWeekday', //現在時刻と曜日を表示
-                  style: const TextStyle(
-                    fontSize: 48,
-                  ),
-                ),
-              ],
-            ),
-            Center(
-              child: Container(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
+          SingleChildScrollView(
+            child: AspectRatio(
+              aspectRatio: MediaQuery.of(context).size.width / MediaQuery.of(context).size.height,
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 16,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      children: [
-                        Image.asset('assets/images/account_icon_selected.png',
-                        width: 64, height: 64), 
-                        const SizedBox(width: 8,),
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(18, 4, 18, 4),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: const Row(
-                            children: [
-                              Text(
-                                '月',
-                                style: TextStyle(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              SizedBox(width: 32),
-                              Text(
-                                '火',
-                                style: TextStyle(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              SizedBox(width: 32),
-                              Text(
-                                '水',
-                                style: TextStyle(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              SizedBox(width: 32),
-                              Text(
-                                '木',
-                                style: TextStyle(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              SizedBox(width: 32),
-                              Text(
-                                '金',
-                                style: TextStyle(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ], 
-                          ),
-                        ),
-                      ],
-                    ), 
-                  ],
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                const Row(children: [Text(''), SizedBox(width: 21,)],),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(16, 35, 16, 35),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '1',
-                            style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 40,),
-                          Text(
-                            '2',
-                            style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 80,),
-                          Text(
-                            '3',
-                            style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 40,),
-                          Text(
-                            '4',
-                            style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
+                    Text(
+                      '$formattedTime $nowWeekday', //現在時刻と曜日を表示
+                      style: const TextStyle(
+                        fontSize: 48,
                       ),
                     ),
                   ],
                 ),
-                const SizedBox(width: 14,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    showsubject('101'),
-                    const SizedBox(height: 2,),
-                    showsubject('102'),
-                    const SizedBox(height: 10,),
-                    showsubject('103'),
-                    const SizedBox(height: 2,),
-                    showsubject('104'),
-                  ],
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/account_icon_selected.png',
+                              width: 64,
+                              height: 64,
+                            ),
+                            const SizedBox(width: 8),
+                            Flexible(
+                              child: Container(
+                                padding: const EdgeInsets.fromLTRB(4, 4, 12, 4),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Spacer(flex: 1,),
+                                    Flexible(
+                                      child: Text(
+                                        '月',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                    Spacer(flex: 2,),
+                                    Flexible(
+                                      child: Text(
+                                        '火',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                    Spacer(flex: 2,),
+                                    Flexible(
+                                      child: Text(
+                                        '水',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                    Spacer(flex: 2,),
+                                    Flexible(
+                                      child: Text(
+                                        '木',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                    Spacer(flex: 2,),
+                                    Flexible(
+                                      child: Text(
+                                        '金',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                    Spacer(flex: 1,),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                const SizedBox(width: 2,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
                   children: [
-                    showsubject('201'),
-                    const SizedBox(height: 2,),
-                    showsubject('202'),
-                    const SizedBox(height: 10,),
-                    showsubject('203'),
-                    const SizedBox(height: 2,),
-                    showsubject('204'),
-                  ],
-                ),
-                const SizedBox(width: 2,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    showsubject('301'),
-                    const SizedBox(height: 2,),
-                    showsubject('302'),
-                    const SizedBox(height: 10,),
-                    showsubject('303'),
-                    const SizedBox(height: 2,),
-                    showsubject('304'),
-                  ],
-                ),
-                const SizedBox(width: 2,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    showsubject('401'),
-                    const SizedBox(height: 2,),
-                    showsubject('402'),
-                    const SizedBox(height: 10,),
-                    showsubject('403'),
-                    const SizedBox(height: 2,),
-                    showsubject('404'),
-                  ],
-                ),
-                const SizedBox(width: 2,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    showsubject('501'),
-                    const SizedBox(height: 2,),
-                    showsubject('502'),
-                    const SizedBox(height: 10,),
-                    showsubject('503'),
-                    const SizedBox(height: 2,),
-                    showsubject('504'),
+                    const Row(children: [Text(''), SizedBox(width: 21,)],),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(16, 35, 16, 35),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '1',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 40,),
+                              Text(
+                                '2',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 80,),
+                              Text(
+                                '3',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 40,),
+                              Text(
+                                '4',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 14,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        showsubject('101'),
+                        const SizedBox(height: 2,),
+                        showsubject('102'),
+                        const SizedBox(height: 10,),
+                        showsubject('103'),
+                        const SizedBox(height: 2,),
+                        showsubject('104'),
+                      ],
+                    ),
+                    const SizedBox(width: 2,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        showsubject('201'),
+                        const SizedBox(height: 2,),
+                        showsubject('202'),
+                        const SizedBox(height: 10,),
+                        showsubject('203'),
+                        const SizedBox(height: 2,),
+                        showsubject('204'),
+                      ],
+                    ),
+                    const SizedBox(width: 2,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        showsubject('301'),
+                        const SizedBox(height: 2,),
+                        showsubject('302'),
+                        const SizedBox(height: 10,),
+                        showsubject('303'),
+                        const SizedBox(height: 2,),
+                        showsubject('304'),
+                      ],
+                    ),
+                    const SizedBox(width: 2,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        showsubject('401'),
+                        const SizedBox(height: 2,),
+                        showsubject('402'),
+                        const SizedBox(height: 10,),
+                        showsubject('403'),
+                        const SizedBox(height: 2,),
+                        showsubject('404'),
+                      ],
+                    ),
+                    const SizedBox(width: 2,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        showsubject('501'),
+                        const SizedBox(height: 2,),
+                        showsubject('502'),
+                        const SizedBox(height: 10,),
+                        showsubject('503'),
+                        const SizedBox(height: 2,),
+                        showsubject('504'),
+                      ],
+                    ),
                   ],
                 ),
               ],
+                      ),
             ),
-          ],
-        ),
+          ),
         ],
       ),
     );
@@ -287,6 +314,9 @@ class _MyWidgetState extends State<Timetable> with SingleTickerProviderStateMixi
           String teacher2 = subjectDoc['teacher2'].toString();
           String roomname = subjectDoc['roomname'].toString();
 
+          double sizewidth = MediaQuery.of(context).size.width * 0.14;
+          double sizeheight = MediaQuery.of(context).size.height * 0.12;
+
           return Container(
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
@@ -299,8 +329,8 @@ class _MyWidgetState extends State<Timetable> with SingleTickerProviderStateMixi
                   width: 1,
                 ),
                 padding: EdgeInsets.zero,
-                minimumSize: const Size(60, 100),
-                maximumSize: const Size(60, 100),
+                minimumSize: Size(sizewidth, sizeheight),
+                maximumSize: Size(sizewidth, sizeheight),
               ),
               onPressed: () {
                 showDialog(
